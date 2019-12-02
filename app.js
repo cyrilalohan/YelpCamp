c93c0696da02502c7bd4c05cef5272ce9561f4af
Osarumen Alohan <cyrilalohan@gmail.com>
2019-12-02 09:45:43 +0000

    change listen port

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
-app.listen(3000, () => {
-	console.log("SERVER CONNECTED!");
+app.listen(process.env.PORT || 3000, () => {
+	console.log("SERVER STARTED!");
 });
